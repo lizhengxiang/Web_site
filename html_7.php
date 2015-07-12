2ca5d0d27fbce86bcf32678f18c50d29ed036fdc
lizhengxiang <1065482100@qq.com>
2015-07-12 09:02:04 +0800

    html 7

---
+++ html_7.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+<meta charset = "utf-8">
+<title>this is frist web</title>
+</head>
+<body>
+	<center> <h1> lizhengxiang </h1></center>
+	<img src = "/images/sister.jpg" width = "400" height = "400" alt = "download file" title = "liyiying">
+	<ul>
+		<li>lizhengxiang</li>
+		<li>lizhenqian</li>
+		<ul>
+			<li>hello world<li>
+		</ul>
+		<li>lizhengxiang<li>
+	</ul>
+	<br>
+	<ol>
+		<li><h1>first<h1></li>
+		<li><h1>second<h1></li>
+	<ol>
+</body>
+</html>
