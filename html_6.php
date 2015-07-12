5ec4acd604faab0503cb687a442e6fbdb88bfd62
lizhengxiang <1065482100@qq.com>
2015-07-12 08:50:59 +0800

    html 6

---
+++ html_6.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+<meta charset = "utf-8">
+<title>this is frist web</title>
+</head>
+<body>
+	<center> <h1> lizhengxiang </h1></center>
+	<img src = "/images/sister.jpg" width = "400" height = "400" alt = "download file" title = "liyiying">
+</body>
+</html>
