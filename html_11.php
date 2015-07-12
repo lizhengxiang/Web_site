c10a66b1d39b1fc99b2a9891fd32ca26792ed97c
lizhengxiang <1065482100@qq.com>
2015-07-12 14:43:43 +0800

    html 11

---
+++ html_11.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+<head>
+<meta charset = "utf-8" >
+<title> user login</title>
+</head>
+<body>
+<form action = "/html_10.php" method = "post">
+<input type = "radio" name = "Profession", value = "Profession1"> lizhengxiang <br>
+<input type = "radio" name = "Profession", value = "Profession2"> lizhengxiang <br>
+<input type = "radio" name = "Profession", value = "Profession3"> lizhengxiang <br>
+<input type = "radio" name = "Profession", value = "Profession4"> lizhengxiang <br>
+<input type = "checkbox" name = "Profession1", value = "Profession1"> lizhengxiang <br>
+<input type = "checkbox" name = "Profession1", value = "Profession2"> lizhengxiang <br>
+<select name = "select" size = "4" multiple>
+<option name = "Profession1">   lizhengxiang1
+<option name = "Profession1">   lizhengxiang2
+<option name = "Profession1">   lizhengxiang3
+<option name = "Profession1">   lizhengxiang4
+<option name = "Profession1">   lizhengxiang5
+<option name = "Profession1">   lizhengxiang6
+<option name = "Profession1">   lizhengxiang7
+</select>
+</br>
+<input list = "car">
+<datalist id = "car">
+<option value = "B M W">
+<option value = "B M W">
+<option value = "B M W">
+<option value = "B M W">
+<option value = "B M W">
+<option value = "B M W">
+<option value = "B M W">
+</datalist>
+</br>
+</br></br>
+<input type = "button" name = "button" value = "jump" onClick = "/html_10.php">
+</br></br>
+
+user massage</br>
+User<br>
+<input type = "text" name = "user", size = "20", maxlength = "15", value = "lizhengxiang">	
+<br>
+password</br>
+<input type = "password", name = "password">
+</br>
+<input type = "submit" , value = "login">
+</form>
+</body>
+</html>
