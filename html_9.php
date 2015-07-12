3780369517ff4d7ce11c1848d5176f796d4e7f6f
lizhengxiang <1065482100@qq.com>
2015-07-12 13:47:11 +0800

    html 9

---
+++ html_9.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+<head>
+<meta charset = "utf-8">
+<title>this is frist web</title>
+</head>
+<body>
+	<center> <h1> lizhengxiang </h1></center>
+	<a href = "/html_8.php" accesskey = "z"><img src = "/images/sister.jpg" width = "400" height = "400" alt = "download file" usemap = "#Map"title = "liyiying"></a>
+
+<!--<img src = "/images/sister.jpg" border = "0" usemap = "#Map"title = "liyiying">-->
+	<map name = "Map" id = "Map">
+		<area shape = "rect" coords = "-3, -1, 131, 138" hear = "html_8.php" />
+	</map>
+	<ul>
+		<li>lizhengxiang</li>
+		<li>lizhenqian</li>
+		<ul>
+			<li>hello world<li>
+		</ul>
+		<li>lizhengxiang<li>
+	</ul>
+	<br>
+	<ol>
+		<li><h1>first<h1></li>
+		<li><h1>second<h1></li>
+	<ol>
+</body>
+</html>
