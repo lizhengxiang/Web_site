4af16c79ceb205cc753ff5f1eab3149c75ada3cb
lizhengxiang <1065482100@qq.com>
2015-07-12 09:42:52 +0800

    html 8

---
+++ html_8.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+<meta charset = "utf-8">
+<title>this is frist web</title>
+</head>
+<body>
+	<p style = "word-spacing:normal"> Welcome to chengdu! </p>
+	<p style = "word-spacing:15px" > Welcome to chengdu!</p>
+	<p style = "letter-spacing:normal">Welcome to chengdu!</p>
+	<p style = "letter-spacing:10px">Welcome to chengdu! </p>
+
+	<p style = "text-decoration:underline"> Welcome to chengdu! </p>
+ 	<p style = "text-decoration:overline" > Welcome to chengdu!</p>
+	<p style = "text-decoration:line-through">Welcome to chengdu!</p>
+	<p style = "text-decoration:blink">Welcome to chengdu! </p>
+	<p style = "text-decoration:underline"> Welcome to chengdu! </p>
+   
+	<p style = "text-decoration:overline" > 
+		Welcome<b style = "font-size:8pt; vertical-align:sub">2012</b> to chengdu!
+	</p>
+    <p style = "text-decoration:line-through">
+		Welcome<b style = "font-size:8pt; vertical-align:top">2012</b> to chengdu!
+	</p>
+    <p style = "text-decoration:blink">
+		Welcome<b style = "font-size:8pt; vertical-align:bottom">2012</b> to chengdu!
+	</p>
+	
+	<p style = "line-height:3"> Welcome <br> to <br> chengdu! </p>
+	<p style = "line-height:70%"> Welcome <br> to <br> chengdu! </p>
+
+</body>
+</html>
