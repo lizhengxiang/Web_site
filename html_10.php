eaafecf9319bac1b1ed305b8614a90f1c6179238
lizhengxiang <1065482100@qq.com>
2015-07-12 14:10:25 +0800

    html 10

---
+++ html_10.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset = "utf-8" >
+<title> user login</title>
+</head>
+<body>
+<form>
+user massage</br>
+User<br>
+<input type = "text" name = "user", size = "20", maxlength = "15", value = "lizhengxiang">	
+<br>
+password</br>
+<input type = "password", name = "password">
+</br>
+<input type = "submit" , value = "login">
+</form>
+</body>
+</html>
