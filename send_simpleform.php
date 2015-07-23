bb96ba609d4115f1788b5e557a37fabb02acfc03
lizhengxiang <1065482100@qq.com>
2015-07-23 18:26:44 +0800

    html and php

---
+++ send_simpleform.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> A simple response</title>
+</head>
+<body>
+	<p><h1>welcome, <strong><?php echo $_GET['user']; ?><strong>!</h1></p>
+	<p><h1>your message is: <strong><?php echo $_GET['message']; ?></strong></h1></p>
+</body>
+</html>
