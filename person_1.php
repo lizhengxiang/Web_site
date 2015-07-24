f08b0a36e9340f8a58a4bc2716de6c20e10d5442
lizhengxiang <1065482100@qq.com>
2015-07-24 14:26:51 +0800

    sql

---
+++ person_1.php
@@ -0,0 +1,28 @@
+<?php
+$con = mysql_connect("localhost","root","");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+mysql_select_db("lzx", $con);
+
+$result = mysql_query("SELECT * FROM Persons");
+
+echo "<table border='1'>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>";
+
+while($row = mysql_fetch_array($result))
+  {
+  echo "<tr>";
+  echo "<td>" . $row['FirstName'] . "</td>";
+  echo "<td>" . $row['LastName'] . "</td>";
+  echo "</tr>";
+  }
+echo "</table>";
+
+mysql_close($con);
+?>
